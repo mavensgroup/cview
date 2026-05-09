b6ea624e0e5d26de9ca1c71d424304e9e08ed9ed
rudrab <rudrab@users.noreply.github.com>
2026-05-09 02:39:35 +0000

    deploy: ec437da7a56c54826d381acccc3e46c5944ef118

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-99d5944b.js',
+                path_to_root + 'searchindex-63f11321.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
